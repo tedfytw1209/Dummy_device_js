979cd48f065b27332fba2e6008fe06044fe19568
tedfytw1209 <teddy90731@gmail.com>
2020-05-03 10:21:42 +0800

    add new sensor odf
    new sensor odf lux,power,sunshine

--- js/ida.js
+++ js/ida.js
@@ -4,7 +4,8 @@
 		    'dm_name': 'DataServer',
 			'idf_list':[],
 			'odf_list':[CO2_O,Humidity_O1,Humidity_O2,Humidity_O3,Humidity_O4,Humidity_O5,Humidity_O6,Humidity_O7,Humidity_O8,Humidity_O9,
-            Temperature_O1,Temperature_O2,Temperature_O3,Temperature_O4,Temperature_O5,Temperature_O6,Temperature_O7,Temperature_O8,Temperature_O9],
+            Temperature_O1,Temperature_O2,Temperature_O3,Temperature_O4,Temperature_O5,Temperature_O6,Temperature_O7,Temperature_O8,Temperature_O9,
+            Sunshine_O1,Usewater_O1,Usewaterdelta_O1,Powergenerate_O1,LLSLux_O1,LLSLux_O2,LLSLux_O3,LLSLux_O4,LLSLux_O5,PF_O1],
 		        'd_name': 'DataServer_range02',
         };
 		
@@ -48,6 +49,30 @@
            $('.Temperature_O8')[0].innerText=data[0];}
         function Temperature_O9(data){
            $('.Temperature_O9')[0].innerText=data[0];}
+        //4/23add
+        function Sunshine_O1(data){
+           $('.Sunshine_O1')[0].innerText=data[0];}
+        function Usewater_O1(data){
+           $('.Usewater_O1')[0].innerText=data[0];}
+        function Usewaterdelta_O1(data){
+           $('.Usewaterdelta_O1')[0].innerText=data[0];}
+        function Powergenerate_O1(data){
+           $('.Powergenerate_O1')[0].innerText=data[0];}
+        function LLSLux_O1(data){
+           $('.LLSLux_O1')[0].innerText=data[0];}
+        function LLSLux_O2(data){
+           $('.LLSLux_O2')[0].innerText=data[0];}
+        function LLSLux_O3(data){
+           $('.LLSLux_O3')[0].innerText=data[0];}
+        function LLSLux_O4(data){
+           $('.LLSLux_O4')[0].innerText=data[0];}
+        function LLSLux_O5(data){
+           $('.LLSLux_O5')[0].innerText=data[0];}
+        function PF_O1(data){
+           $('.PF_O1')[0].innerText=data[0];}
+        
+        
+
 /*******************************************************************/                
         function ida_init(){
 	    console.log(profile.d_name);
