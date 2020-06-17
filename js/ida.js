f5efe7d3448d018a86f624b0e65be920b53838cd
tedfytw1209 <teddy90731@gmail.com>
2020-06-17 15:26:18 +0800

    add if to avoid no class error

--- js/ida.js
+++ js/ida.js
@@ -19,111 +19,161 @@
             return val.toFixed(decimals);
         }
         function CO2_O(data){
+            if ($('.CO2_O')[0]){
            $('.CO2_O')[0].innerText=data[0];
             //console.log('data:', data[0]);
-           }
+           }}
         function Humidity_O1(data){
-           $('.Humidity_O1')[0].innerText=data[0];}
+            if ($('.Humidity_O1')[0]){
+           $('.Humidity_O1')[0].innerText=data[0];}}
         function Humidity_O2(data){
-           $('.Humidity_O2')[0].innerText=data[0];}
+            if ($('.Humidity_O2')[0]){
+           $('.Humidity_O2')[0].innerText=data[0];}}
         function Humidity_O3(data){
-           $('.Humidity_O3')[0].innerText=data[0];}
+            if ($('.Humidity_O3')[0]){
+           $('.Humidity_O3')[0].innerText=data[0];}}
         function Humidity_O4(data){
-           $('.Humidity_O4')[0].innerText=data[0];}
+            if ($('.Humidity_O4')[0]){
+           $('.Humidity_O4')[0].innerText=data[0];}}
         function Humidity_O5(data){
-           $('.Humidity_O5')[0].innerText=data[0];}
+            if ($('.Humidity_O5')[0]){
+           $('.Humidity_O5')[0].innerText=data[0];}}
         function Humidity_O6(data){
-           $('.Humidity_O6')[0].innerText=data[0];}
+            if ($('.Humidity_O6')[0]){
+           $('.Humidity_O6')[0].innerText=data[0];}}
         function Humidity_O7(data){
-           $('.Humidity_O7')[0].innerText=data[0];}
+            if ($('.Humidity_O7')[0]){
+           $('.Humidity_O7')[0].innerText=data[0];}}
         function Humidity_O8(data){
-           $('.Humidity_O8')[0].innerText=data[0];}
+            if ($('.Humidity_O8')[0]){
+           $('.Humidity_O8')[0].innerText=data[0];}}
         function Humidity_O9(data){
-           $('.Humidity_O9')[0].innerText=data[0];}
+            if ($('.Humidity_O9')[0]){
+           $('.Humidity_O9')[0].innerText=data[0];}}
         function Temperature_O1(data){
-           $('.Temperature_O1')[0].innerText=data[0];}
+            if ($('.Temperature_O1')[0]){
+           $('.Temperature_O1')[0].innerText=data[0];}}
         function Temperature_O2(data){
-           $('.Temperature_O2')[0].innerText=data[0];}
+            if ($('.Temperature_O2')[0]){
+           $('.Temperature_O2')[0].innerText=data[0];}}
         function Temperature_O3(data){
-           $('.Temperature_O3')[0].innerText=data[0];}
+            if ($('.Temperature_O3')[0]){
+           $('.Temperature_O3')[0].innerText=data[0];}}
         function Temperature_O4(data){
-           $('.Temperature_O4')[0].innerText=data[0];}
+            if ($('.Temperature_O4')[0]){
+           $('.Temperature_O4')[0].innerText=data[0];}}
         function Temperature_O5(data){
-           $('.Temperature_O5')[0].innerText=data[0];}
+            if ($('.Temperature_O5')[0]){
+           $('.Temperature_O5')[0].innerText=data[0];}}
         function Temperature_O6(data){
-           $('.Temperature_O6')[0].innerText=data[0];}
+            if ($('.Temperature_O6')[0]){
+           $('.Temperature_O6')[0].innerText=data[0];}}
         function Temperature_O7(data){
-           $('.Temperature_O7')[0].innerText=data[0];}
+            if ($('.Temperature_O7')[0]){
+           $('.Temperature_O7')[0].innerText=data[0];}}
         function Temperature_O8(data){
-           $('.Temperature_O8')[0].innerText=data[0];}
+            if ($('.Temperature_O8')[0]){
+           $('.Temperature_O8')[0].innerText=data[0];}}
         function Temperature_O9(data){
-           $('.Temperature_O9')[0].innerText=data[0];}
+            if ($('.Temperature_O9')[0]){
+           $('.Temperature_O9')[0].innerText=data[0];}}
         //range odf
         function CO2range_O(data){
+            if ($('.CO2range_O')[0]){
            $('.CO2range_O')[0].innerText=data[0];
             //console.log('data:', data[0]);
-           }
+           }}
         function Humidityrange_O1(data){
-           $('.Humidityrange_O1')[0].innerText=data[0];}
+            if ($('.Humidityrange_O1')){
+           $('.Humidityrange_O1')[0].innerText=data[0];}}
         function Humidityrange_O2(data){
-           $('.Humidityrange_O2')[0].innerText=data[0];}
+            if ($('.Humidityrange_O2')[0]){
+           $('.Humidityrange_O2')[0].innerText=data[0];}}
         function Humidityrange_O3(data){
-           $('.Humidityrange_O3')[0].innerText=data[0];}
+            if ($('.Humidityrange_O3')[0]){
+           $('.Humidityrange_O3')[0].innerText=data[0];}}
         function Humidityrange_O4(data){
-           $('.Humidityrange_O4')[0].innerText=data[0];}
+            if ($('.Humidityrange_O4')[0]){
+           $('.Humidityrange_O4')[0].innerText=data[0];}}
         function Humidityrange_O5(data){
-           $('.Humidityrange_O5')[0].innerText=data[0];}
+            if ($('.Humidityrange_O5')[0]){
+           $('.Humidityrange_O5')[0].innerText=data[0];}}
         function Humidityrange_O6(data){
-           $('.Humidityrange_O6')[0].innerText=data[0];}
+            if ($('.Humidityrange_O6')[0]){
+           $('.Humidityrange_O6')[0].innerText=data[0];}}
         function Humidityrange_O7(data){
-           $('.Humidityrange_O7')[0].innerText=data[0];}
+            if ($('.Humidityrange_O7')[0]){
+           $('.Humidityrange_O7')[0].innerText=data[0];}}
         function Humidityrange_O8(data){
-           $('.Humidityrange_O8')[0].innerText=data[0];}
+            if ($('.Humidityrange_O8')[0]){
+           $('.Humidityrange_O8')[0].innerText=data[0];}}
         function Humidityrange_O9(data){
-           $('.Humidityrange_O9')[0].innerText=data[0];}
+            if ($('.Humidityrange_O9')[0]){
+           $('.Humidityrange_O9')[0].innerText=data[0];}}
         function Temperaturerange_O1(data){
-           $('.Temperaturerange_O1')[0].innerText=data[0];}
+            if ($('.Temperaturerange_O1')[0]){
+           $('.Temperaturerange_O1')[0].innerText=data[0];}}
         function Temperaturerange_O2(data){
-           $('.Temperaturerange_O2')[0].innerText=data[0];}
+            if ($('.Temperaturerange_O2')[0]){
+           $('.Temperaturerange_O2')[0].innerText=data[0];}}
         function Temperaturerange_O3(data){
-           $('.Temperaturerange_O3')[0].innerText=data[0];}
+            if ($('.Temperaturerange_O3')[0]){
+           $('.Temperaturerange_O3')[0].innerText=data[0];}}
         function Temperaturerange_O4(data){
-           $('.Temperaturerange_O4')[0].innerText=data[0];}
+            if ($('.Temperaturerange_O4')[0]){
+           $('.Temperaturerange_O4')[0].innerText=data[0];}}
         function Temperaturerange_O5(data){
-           $('.Temperaturerange_O5')[0].innerText=data[0];}
+            if ($('.Temperaturerange_O5')[0]){
+           $('.Temperaturerange_O5')[0].innerText=data[0];}}
         function Temperaturerange_O6(data){
-           $('.Temperaturerange_O6')[0].innerText=data[0];}
+            if ($('.Temperaturerange_O6')[0]){
+           $('.Temperaturerange_O6')[0].innerText=data[0];}}
         function Temperaturerange_O7(data){
-           $('.Temperaturerange_O7')[0].innerText=data[0];}
+            if ($('.Temperaturerange_O7')[0]){
+           $('.Temperaturerange_O7')[0].innerText=data[0];}}
         function Temperaturerange_O8(data){
-           $('.Temperaturerange_O8')[0].innerText=data[0];}
+            if ($('.Temperaturerange_O8')[0]){
+           $('.Temperaturerange_O8')[0].innerText=data[0];}}
         function Temperaturerange_O9(data){
-           $('.Temperaturerange_O9')[0].innerText=data[0];}
+            if ($('.Temperaturerange_O9')[0]){
+           $('.Temperaturerange_O9')[0].innerText=data[0];}}
         //4/23add
         function Sunshine_O1(data){
-           $('.Sunshine_O1')[0].innerText=data[0];}
+            if ($('.Sunshine_O1')[0]){
+           $('.Sunshine_O1')[0].innerText=data[0];}}
         function Usewater_O1(data){
-           $('.Usewater_O1')[0].innerText=number_format(data[0],1);}
+            if ($('.Usewater_O1')[0]){
+           $('.Usewater_O1')[0].innerText=number_format(data[0],1);}}
         function Usewaterdelta_O1(data){
-           $('.Usewaterdelta_O1')[0].innerText=number_format(data[0],1);}
+            if ($('.Usewaterdelta_O1')[0]){
+           $('.Usewaterdelta_O1')[0].innerText=number_format(data[0],1);}}
         function Powergenerate_O1(data){
-           $('.Powergenerate_O1')[0].innerText=data[0];}
+            if ($('.Powergenerate_O1')[0]){
+           $('.Powergenerate_O1')[0].innerText=data[0];}}
         function Lux_O1(data){
-           $('.Lux_O1')[0].innerText=data[0];}
+            if ($('.Lux_O1')[0]){
+           $('.Lux_O1')[0].innerText=data[0];}}
         function Lux_O2(data){
-           $('.Lux_O2')[0].innerText=data[0];}
+            if ($('.Lux_O2')[0]){
+           $('.Lux_O2')[0].innerText=data[0];}}
         function Lux_O3(data){
-           $('.Lux_O3')[0].innerText=data[0];}
+            if ($('.Lux_O3')[0]){
+           $('.Lux_O3')[0].innerText=data[0];}}
         function Lux_O4(data){
-           $('.Lux_O4')[0].innerText=data[0];}
+            if ($('.Lux_O4')[0]){
+           $('.Lux_O4')[0].innerText=data[0];}}
         function Lux_O5(data){
-           $('.Lux_O5')[0].innerText=data[0];}
+            if ($('.Lux_O5')[0]){
+           $('.Lux_O5')[0].innerText=data[0];}}
         function PF_O1(data){
-           $('.PF_O1')[0].innerText=number_format(data[0],2);}
+            if ($('.PF_O1')[0]){
+            $('.PF_O1')[0].innerText=number_format(data[0],2);}}
         function WaterLevel_O1(data){
-           $('.WaterLevel_O1')[0].innerText=number_format(data[0],1);}
+            if ($('.WaterLevel_O1')[0]){
+            $('.WaterLevel_O1')[0].innerText=number_format(data[0],1);}}
         function WaterLevel_O2(data){
-           $('.WaterLevel_O2')[0].innerText=number_format(data[0],1);}
+            if ($('.WaterLevel_O2')[0]){
+            $('.WaterLevel_O2')[0].innerText=number_format(data[0],1);}}
 
 /*******************************************************************/                
         function ida_init(){
